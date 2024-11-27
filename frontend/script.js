9be4de09fa6528574ae0528fcf00a7d99325362c
Luiz Henrique <luiz.henriqueoliveira@eaportal.org>
2024-11-27 20:48:15 -0300

    finalizado

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+function verifyToken() {
+    const token = localStorage.getItem("token")
+
+    if(!token) {
+        window.location.href = "./pages/login/login.html"
+        return
+    }
+
+    //verificar se o token é valido
+}
+
+verifyToken()
